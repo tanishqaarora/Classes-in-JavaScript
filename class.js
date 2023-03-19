7d75854049320940950d86bec9f5b8f7ee0eb1d5
Tanishqa Arora <tanishqa164@gmail.com>
2023-03-19 22:49:26 +0530

    new commit

--- class.js
+++ class.js
@@ -1,30 +1,4 @@
 
 
-// 2. By using: class keyword in JS
-
-// Here, getOne added into the constructor
-// sayHi added into the prototype
-
-class User {
-
-    constructor(name) {
-        this.name = name,
-        this.getOne = function(){
-            console.log("1");
-        }
-    }
-    // add new function
-    sayHi() {
-        console.log("Hello there");
-    }
-
-}
-
-const user1 = new User("Mike");       // created a new object of class User
-console.log(user1);                   // this will not contain sayHi() [the prototype property]
-
-// To access the prototype property, call it separately on the user object you want to use.
-user1.sayHi();
-
 
 
--- constructorAndClass.js
+++ constructorAndClass.js
@@ -23,3 +23,30 @@ User.prototype.sayHi = function() {
 console.log(user1);                // this will not show the function added in the prototype
 // user1.sayHi(); 
 // user1.one()                    // we can access the prototype function by calling it separately
+
+
+// 2. By using: class keyword in JS
+
+// Here, getOne added into the constructor
+// sayHi added into the prototype
+
+class User {
+
+    constructor(name) {
+        this.name = name,
+        this.getOne = function(){
+            console.log("1");
+        }
+    }
+    // add new function
+    sayHi() {
+        console.log("Hello there");
+    }
+
+}
+
+const user2 = new User("Mike");       // initiate an object of class User
+console.log(user2);                   // this will not contain sayHi() [the prototype property]
+
+// To access the prototype property, call it separately on the user object you want to use.
+user2.sayHi();
