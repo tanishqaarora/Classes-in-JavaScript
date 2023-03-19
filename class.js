d38785b553d1ca1cae991ccafbbef56f297b2451
Tanishqa Arora <tanishqa164@gmail.com>
2023-03-19 22:38:57 +0530

    small changes added

--- class.js
+++ class.js
@@ -2,10 +2,16 @@
 
 // 2. By using: class keyword in JS
 
+// Here, getOne added into the constructor
+// sayHi added into the prototype
+
 class User {
 
     constructor(name) {
-        this.name = name
+        this.name = name,
+        this.getOne = function(){
+            console.log("1");
+        }
     }
     // add new function
     sayHi() {
@@ -14,9 +20,11 @@ class User {
 
 }
 
-const user2 = new User("Mike");
-console.log(user2);
-user2.sayHi();
+const user1 = new User("Mike");       // created a new object of class User
+console.log(user1);                   // this will not contain sayHi() [the prototype property]
+
+// To access the prototype property, call it separately on the user object you want to use.
+user1.sayHi();
 
 
 
--- constructorFunc.js
+++ constructorFunc.js
@@ -4,17 +4,22 @@
 // 1. By using: User Construnctor function
 
 function User(name) {
-    this.name = name
+    this.name = name,
+    this.getOne = function(){
+        console.log("1");
+    }
 }
 
 const user1 = new User("Harry");
-console.log(user1);
 
-// Add a new method (a function) to this constructor
-// this can be done by adding the function  into the prototype property of User
+// Now, we can new property (a function) either into the constructor or inside the prototype
+// But we don't see the added function in the "User" object if function is added inside the prototype
+// Let's add the function "sayHi" into the prototype property of User
 
 User.prototype.sayHi = function() {
     console.log("Hello there");
 }
 
-user1.sayHi();
+console.log(user1);                // this will not show the function added in the prototype
+// user1.sayHi(); 
+// user1.one()                    // we can access the prototype function by calling it separately
