3422fc20e3b8687a7ae2f83d1cfc6aa0f9d10a1a
Tanishqa Arora <tanishqa164@gmail.com>
2023-03-20 11:46:46 +0530

    created a new class

--- class.js
+++ class.js
@@ -1,4 +1,31 @@
+class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 
+    getSummary(){
+        return `${this.title} was written by ${this.author} in year ${this.year}`;
+    }
+
+    getAge(){
+        const years = new Date().getFullYear() - this.year;
+        return `${this.title} is ${years} old`;
+    }
+    
+    revise(newYear) {
+        this.year = newYear;
+        this.revised = true;
+    }
+}
+
+// Initiate Object
+
+const book1 = new Book('Book One', 'John Doe', '2013');
+console.log(book1);
+book1.revise('2018');
+console.log(book1);
 
 
 
